83ff3f543d7d54e417e28cdea5bbef4590940c05
Sikas744 <Sikasreddy369@gmail.com>
2023-03-03 11:11:16 +0530

    my first commit

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+ public static void main(String[] args)
+[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C{
+  System.out.println("Hello,World");
+  }
+ }

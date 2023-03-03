094c4fa3880e87641f747aeceb0668929305eb8e
Sikas SK <94596188+Sikas744@users.noreply.github.com>
2023-03-03 11:14:02 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
 public class Hello{
- public static void main(String[] args)
-[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C{
+ public static void main(String[] args){
   System.out.println("Hello,World");
   }
  }
